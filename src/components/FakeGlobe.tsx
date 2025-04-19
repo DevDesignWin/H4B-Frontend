d6b85f8d2daf53b994bf0cb89746eb3def3713d7
DEBOJIT ROY <146013408+DevDesignWin@users.noreply.github.com>
2025-04-20 00:01:03 +0530

    Update FakeGlobe.tsx

--- src/components/FakeGlobe.tsx
+++ src/components/FakeGlobe.tsx
@@ -1,6 +1,6 @@
 'use client';
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { OrbitControls, Stars, useTexture, useHelper } from '@react-three/drei';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls, Stars, useTexture } from '@react-three/drei';
 import * as THREE from 'three';
 import { useRef, Suspense } from 'react';
 
